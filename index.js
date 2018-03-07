e263bf6c084674c1058078cdf5927af5f2443dc5
Sufiane Souissi <sufiane.souissi@ekino.com>
2018-03-07 18:08:46 +0100

    final commit

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 const data = require('./data.js');
 const args = process.argv;
 
+const getSecretKey = function(givenEndpoint){
+    const endpointIndex = data.endpoints.indexOf(givenEndpoint)
+
+    if (endpointIndex > -1) {
+        // Since all the keys are string it's safe to use keys
+        const secretKeys = Object.keys(data.keys)
+
+        return secretKeys[endpointIndex]
+    }
+
+    return 'Error'
+};
+
 if(args.length > 2){
   let endpoints = args.slice(2, args.length);
   for(let i in endpoints){
@@ -13,10 +26,6 @@ else{
   console.log('you must specify an endpoint');
   process.exit(0);
 }
-
-const getSecretKey = function(endpoint){
-  //TODO
-};
 
 
 
